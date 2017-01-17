f5ac004f0a8c7dbfb12108e70bbabbdd58c99c8a
Hyoungjin Bae <impazziamoci@gmail.com>
2017-01-17 17:53:00 +0900

    HelloSmile folder

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/hello")		//templates 폴더에 있는 hello 파일로 연결된다.
+	public String hello(String name, int age, Model model){
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		System.out.println(name);
+		return "hello";	
+	}
+}
